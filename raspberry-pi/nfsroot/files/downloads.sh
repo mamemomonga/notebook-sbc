e126fe3b766b89101d40e22de89b69f083fb7368
mamemomonga <mamemomonga@gmail.com>
2020-11-05 10:26:40 +0900

    Update files

--- raspberry-pi/nfsroot/files/downloads.sh
+++ raspberry-pi/nfsroot/files/downloads.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
 set -eu
 
 FILES_URL="https://raw.githubusercontent.com/mamemomonga/notebook-sbc/master/raspberry-pi/nfsroot/files"
 
 download() {
-	local name=$1
-	local fexec=${2:-}
-	mkdir -p files
-	curl -Lo files/$name $FILES_URL"/"$name
-	if [ "$fexec" == "1" ]; then
-		chmod files/$name
-	fi
+        local name=$1
+        local fexec="${2:-}"
+        mkdir -p files
+        echo "Download: $name"
+        curl -Lso files/$name $FILES_URL"/"$name
+        if [ "$fexec" == "1" ]; then
+                chmod 755 files/$name
+        fi
 }
 
 download dnsmasq-resolv.conf
@@ -18,4 +18,4 @@ download dnsmasq.conf
 download expand-image.sh 1
 download firewall.service
 download firewall.sh 1
-download piinfo.pl
+
