72b8cfd211651924b689e91e595924c8c4bfa7e7
mamemomonga <mamemomonga@gmail.com>
2020-11-05 10:21:54 +0900

    update files

--- raspberry-pi/nfsroot/files/download.sh
+++ raspberry-pi/nfsroot/files/download.sh
@@ -8,14 +8,13 @@ download() {
 	local fexec=${2:-}
 	mkdir -p files
 	curl -Lo files/$name $FILES_URL"/"$name
-	if [ "$fexec" == 1 ]; then
+	if [ "$fexec" == "1" ]; then
 		chmod files/$name
 	fi
 }
 
 download dnsmasq-resolv.conf
 download dnsmasq.conf
-download download.sh 1
 download expand-image.sh 1
 download firewall.service
 download firewall.sh 1
